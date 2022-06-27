f61956152341e4670f6db36e55456c0f64176609
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-06-27 16:02:47 -0500

    set up the calendar and ite elements

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  let task1 = $("#task-1");
+  let task2 = $("#task-2");
+  let task3 = $("#task-3");
+  let task4 = $("#task-4");
+  let task5 = $("#task-5");
+  let task6 = $("#task-6");
+  let task7 = $("#task-7");
+  let task8 = $("#task-8");
+  let task9 = $("#task-9");
+  let task10 = $("#task-10");
+});
